9035f65347fd23fb554cdafd96be1a58b9f56d07
myenong <enong@yeah.net>
2018-05-17 15:38:07 +0800

    链接问题处理
    链接问题处理

--- report/urls.py
+++ report/urls.py
@@ -7,9 +7,9 @@ api_router = routers.DefaultRouter()
 
 urlpatterns = [
     url(r'^list/', views.excel_export, name='list'),
-    url(r'^viewreport/', views.view_report, name='view_report'),
-    url(r'^viewreport/xiaoshou', views.view_report_xs, name='view_report_xs'),
-    url(r'^viewreport/xscharts', views.view_report_xs_charts, name='view_report_xs_charts'),
-    url(r'^viewreport/xscolumn', views.view_report_xs_column, name='view_report_xs_column'),
+    url(r'^viewreport/$', views.view_report, name='view_report'),
+    url(r'^viewreport/xiaoshou/$', views.view_report_xs, name='view_report_xs'),
+    url(r'^viewreport/xscharts/$', views.view_report_xs_charts, name='view_report_xs_charts'),
+    url(r'^viewreport/xscolumn/$', views.view_report_xs_column, name='view_report_xs_column'),
 
 ]
